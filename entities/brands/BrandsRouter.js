db1f43b2b7292174ca402969c146a0bd8fba630f
JeroniLorenzo <jeronilorenzosolano@gmail.com>
2023-02-27 18:10:24 +0100

    añadido middleware isAdmin a la ruta de crear nuevas marcas

--- entities/brands/BrandsRouter.js
+++ entities/brands/BrandsRouter.js
@@ -7,7 +7,7 @@ const isAdmin = require('../../middlewares/isAdmin');
 const BrandsController = require('./BrandsController')
 
 router.get('/getAll', isAdmin, BrandsController.getAllBrands);
-router.post("/newBrand", BrandsController.newBrand);
+router.post("/newBrand", isAdmin, BrandsController.newBrand);
 router.put("/updateBrand", isAdmin, BrandsController.updateBrand);
 router.delete("/deleteBrand", isAdmin, BrandsController.deleteBrand);
 
